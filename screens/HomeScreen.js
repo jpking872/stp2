b005938596b08153aed1ccab8ef82277c7754583
Jonathan King <kingjon90@gmail.com>
2025-11-26 13:41:01 -0500

    Add message

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,7 +2,7 @@ import {Image, StyleSheet, Text, View} from 'react-native';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import React, { useState, useEffect } from 'react';
 import '../utils/global';
-import SkateLink from "../components/SkateLink";
+import SkateText from "../components/SkateText";
 import SkateButton from "../components/SkateButton";
 import {useNavigation} from "@react-navigation/native";
 import {useAuth} from "../context/AuthContext";
@@ -10,7 +10,7 @@ import {useAuth} from "../context/AuthContext";
 function HomeScreen() {
 
     const navigation = useNavigation();
-    const { isAuthenticated, logout } = useAuth();
+    const { isAuthenticated, logout, venueData } = useAuth();
     function gotoLogin() {
         navigation.navigate("Login");
     }
@@ -22,7 +22,11 @@ function HomeScreen() {
     return (
         <View style={styles.container}>
             <Image source={require('../assets/icon.png')} style={styles.headerImage} />
-            { isAuthenticated ? null : (
+            { isAuthenticated ? (
+                <View>
+                    <SkateText style={styles.messageText}>{venueData.message}</SkateText>
+                </View>
+            ) : (
                 <View>
                     <View style={{ marginBottom: 15 }}><SkateButton title="Sign in" color={global.DARK_COLOR} onPress={gotoLogin} disabled={false} /></View>
                     <View><SkateButton title="Create account" color={global.DARK_COLOR} onPress={gotoRegister} disabled={false} /></View>
@@ -45,6 +49,10 @@ const styles = StyleSheet.create({
         height: 320,
         marginBottom: 30,
         backgroundColor:'#222222'
+    },
+    messageText: {
+        fontSize: 18,
+        color: global.DARK_COLOR
     }
 })
 
